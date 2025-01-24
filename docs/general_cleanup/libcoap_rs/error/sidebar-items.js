6e7ee8717d6d743f44bd2a3a6211779c5ad14abb
pulsastrix <pulsastrix@users.noreply.github.com>
2025-01-24 16:52:42 +0000

    deploy: namib-project/libcoap-rs@90c65114bf9dd0fa220d3d3ffac2b67843eed577

---
+++ docs/general_cleanup/libcoap_rs/error/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ContextConfigurationError","EndpointCreationError","IoProcessError","MessageCodeError","MessageConversionError","MessageCreationError","MessageTypeError","OptionCreationError","OptionValueError","RngError","SessionCreationError","SessionGetAppDataError","UnknownOptionError","UriParsingError"]};
---
+++ docs/general_cleanup/trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["libcoap_sys",[["impl&lt;Storage: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"libcoap_sys/struct.__BindgenBitfieldUnit.html\" title=\"struct libcoap_sys::__BindgenBitfieldUnit\">__BindgenBitfieldUnit</a>&lt;Storage&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[464]}
